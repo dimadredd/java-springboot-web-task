176217223133a1d121a8d1eac4820ad33b69a283
Developer <developer@example.com>
2026-04-21 18:51:15 +0500

    git init

---
+++ src/main/java/sorokin/school/springbootwebtask/SpringbootWebTaskApplication.java
@@ -0,0 +1,13 @@
+package sorokin.school.springbootwebtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sorokin/school/springbootwebtask/SpringbootWebTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package sorokin.school.springbootwebtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
